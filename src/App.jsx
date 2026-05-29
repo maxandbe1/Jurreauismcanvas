63a4c473718ff1f1ddde6738e11765f7af3aff86
maxandbe1 <maxandbe1@proton.me>
2026-05-28 20:13:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ export default function App() {
   const [audioUrl, setAudioUrl] = useState("");
 
   return (
-    <div style={{ padding: 20 }}>
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#000",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        paddingTop: 40,
+      }}
+    >
       <UrlInput onUrlSelected={setAudioUrl} />
       <FullAudioPlayer audioUrl={audioUrl} setAudioUrl={setAudioUrl} />
     </div>
